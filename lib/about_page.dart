961ff5eb0888ba7b315ea1e791185f42e6943632
Sujan Rajesh Babu <sujanrajeshb@gmail.com>
2025-11-19 13:08:01 +0000

    previous text replaced with centralised about us page contents

--- lib/about_page.dart
+++ lib/about_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class AboutPage extends StatelessWidget {
   const AboutPage({Key? key}) : super(key: key);
 
@@ -11,23 +10,42 @@ class AboutPage extends StatelessWidget {
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             Text(
-              'Welcome to Union Shop!',
+              'About us',
               style: Theme.of(context).textTheme.headlineMedium,
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 24),
+            const Text(
+              'Welcome to the Union Shop!',
+              textAlign: TextAlign.center,
             ),
             const SizedBox(height: 16),
             const Text(
-              'Union Shop is your one-stop destination for all your shopping needs. We offer a wide range of products, from electronics to fashion, all at competitive prices.',
+              'We’re dedicated to giving you the very best University branded products, with a range of clothing and merchandise available to shop all year round! We even offer an exclusive personalisation service!',
+              textAlign: TextAlign.center,
             ),
             const SizedBox(height: 16),
             const Text(
-              'Our mission is to provide our customers with the best shopping experience possible. We are committed to quality, affordability, and excellent customer service.',
+              'All online purchases are available for delivery or instore collection!',
+              textAlign: TextAlign.center,
             ),
             const SizedBox(height: 16),
             const Text(
-              'Thank you for choosing Union Shop. We look forward to serving you!',
+              'We hope you enjoy our products as much as we enjoy offering them to you. If you have any questions or comments, please don’t hesitate to contact us at hello@upsu.net.',
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 16),
+            const Text(
+              'Happy shopping!',
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 16),
+            const Text(
+              'The Union Shop & Reception Team',
+              textAlign: TextAlign.center,
             ),
           ],
         ),
