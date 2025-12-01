bcde2107766e93afb8bc056e0756c7b1cc13b2f5
Sujan Rajesh Babu <sujanrajeshb@gmail.com>
2025-12-01 15:47:44 +0000

    Use of AI to replace placeholder footer in every product page with updated footer

--- lib/product_page.dart
+++ lib/product_page.dart
@@ -2,6 +2,7 @@ import 'dart:collection';
 import 'package:flutter/material.dart';
 import 'package:union_shop/widgets/union_navbar.dart';
 import 'package:union_shop/cart_state.dart';
+import 'package:union_shop/widgets/footer.dart';
 
 class ProductPage extends StatefulWidget {
   final ProductDetails? initialProduct;
@@ -233,19 +234,7 @@ class _ProductPageState extends State<ProductPage> {
             ),
 
             // Footer
-            Container(
-              width: double.infinity,
-              color: Colors.grey[50],
-              padding: const EdgeInsets.all(24),
-              child: const Text(
-                'Placeholder Footer',
-                style: TextStyle(
-                  color: Colors.grey,
-                  fontSize: 16,
-                  fontWeight: FontWeight.w600,
-                ),
-              ),
-            ),
+            const FooterWidget(),
           ],
         ),
       ),
