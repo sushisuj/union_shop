5fa4b7c136d00fc791c7c232f74d8e1ef6ceeebe
Sujan Rajesh Babu <sujanrajeshb@gmail.com>
2025-12-03 19:06:28 +0000

    refactor: remove footer widget import from checkout page

--- lib/checkout.dart
+++ lib/checkout.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:union_shop/widgets/union_navbar.dart';
-import 'package:union_shop/widgets/footer.dart';
 import 'package:union_shop/cart_state.dart'; // For CartItem and cart logic
 
 class CheckoutPage extends StatefulWidget {
