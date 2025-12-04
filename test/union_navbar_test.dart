1e0ab7a9b310c03c6895d005efa889e3f6a70fe6
Sujan Rajesh Babu <sujanrajeshb@gmail.com>
2025-12-04 15:47:19 +0000

    fix: add ignore_for_file directive for unused_local_variable and prefer_const_constructors in union_navbar_test

--- test/union_navbar_test.dart
+++ test/union_navbar_test.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: unused_local_variable, prefer_const_constructors
+
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:union_shop/widgets/union_navbar.dart';
