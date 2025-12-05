4c44fc5eff66d4dab5302d0737f67eea179d137d
Sujan Rajesh Babu <sujanrajeshb@gmail.com>
2025-12-05 01:05:18 +0000

    fix: update dropdown selection in test to use 'Merchandise' instead of 'Jumpers'

--- test/essentials2_test.dart
+++ test/essentials2_test.dart
@@ -19,10 +19,10 @@ void main() {
     await tester.pumpWidget(
       MaterialApp(home: EssentialsPage2()),
     );
-    // Open dropdown and select 'Jumpers'
+    // Open dropdown and select 'Merchandise'
     await tester.tap(find.byType(DropdownButton<String>));
     await tester.pumpAndSettle();
-    await tester.tap(find.text('Jumpers').last);
+    await tester.tap(find.text('Merchandise').last);
     await tester.pumpAndSettle();
     expect(find.text('Sandwich'), findsNothing);
   });
